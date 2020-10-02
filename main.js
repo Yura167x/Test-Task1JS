5693668d2d2a929a1a1f3b9704f9ece63fb75d27
Yura167x <64233218+Yura167x@users.noreply.github.com>
2020-10-03 02:20:18 +0300

    v2

--- main.js
+++ main.js
@@ -1,18 +1,38 @@
 'use strict';
 
+var b = 5;
+
 var a = prompt('Guess the number from 0 to 9:');
 a = +a;
 
-if (a === 2) {
-    alert('You guessed the number.');
-}
+if( a === b ) {
+    alert('Imposible!');
+} 
 
-while (a !== 2) {
-    alert('You guessed wrong, try again.');
-    var a = prompt('Please enter a number between 0 and 9.');
+while (a < 5) {
+    alert('The number must be larger.');
+    var a = prompt('Guess the number from 0 to 9:');
     a = +a;
-    if (a === 2) {
+    if ( a === b ) {
         alert('You guessed the number.');
         break;
     }
 }
+
+while (a > 5) {
+        alert('The number should be less.');
+        var a = prompt('Guess the number from 0 to 9:');
+        a = +a;
+        while ( a < 5 ) {
+            alert('The number must be larger.');
+            var a = prompt('Guess the number from 0 to 9:');
+            a = +a;
+            continue;
+        } 
+        if ( a === b ) {
+            alert('You guessed the number.');
+            break;
+    } 
+} 
+
+console.log('The hidden number -', a);
